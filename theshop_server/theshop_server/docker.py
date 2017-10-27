3fb037cb11f2dfc6dfd57dc982e049e38ed3ca40
icooker811 <icooker811@gmail.com>
2017-10-28 05:15:55 +0700

    fix

--- theshop_server/theshop_server/docker.py
+++ theshop_server/theshop_server/docker.py
@@ -8,3 +8,19 @@ SECRET_KEY = 't+1v*0e%uz4u4*=v-s40&y$rrtt7s%rl^r)2j^-h_x(l7(bjxb'
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
+from mongoengine import connect, connection
+connection.disconnect()
+connect('test-sensor', host='mongo')
+
+CACHES['default']['LOCATION'] = 'redis://redis:6379/1'
+
+# CELERY SETTINGS
+BROKER_URL = 'redis://redis:6379/0'
+GOOGLE_API_KEY = env('GOOGLE_API_KEY') or ''
+FCM_API_KEY = env('FCM_API_KEY') or ''
+
+FCM_DJANGO_SETTINGS = {
+       "FCM_SERVER_KEY": FCM_API_KEY,
+       "ONE_DEVICE_PER_USER": True,
+       "DELETE_INACTIVE_DEVICES": True,
+}
